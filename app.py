1190ac9cad9fcf257718d4b0232af8b8d1d4ba95
Muhozi Emery <muhozie@gmail.com>
2018-05-24 23:49:30 +0300

    [Chore] Handle 404 error

--- app.py
+++ app.py
@@ -2,24 +2,28 @@
     Main file to run the app
 """
 import os
-from flask import jsonify
+from flask import jsonify, redirect
 from api import create_app
-from flask_migrate import Migrate
 from api.models import db
+from flask_migrate import Migrate
 APP = create_app(os.getenv('ENV'))
 migrate = Migrate(APP, db)
 
 
-# @APP.errorhandler(404)
-# def page_not_found():
-#     """
-#         Return json error if page not found
-#     """
-#     return jsonify({
-#         'status': 'error',
-#         'message': 'Page not found'
-#     }), 404
+@APP.errorhandler(404)
+def page_not_found(e):
+    """
+        Return json error if page not found
+    """
+    return jsonify({
+        'status': 'error',
+        'message': 'Page not found'
+    }), 404
 
+
+@APP.route('/')
+def helapi_docslo():
+    return redirect('/api/v1', code=302)
 
 if __name__ == '__main__':
     # Run the application
