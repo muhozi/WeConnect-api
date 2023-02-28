8d16cbfff94c591dd84bf7881f6fd36375a72156
Emery Muhozi <muhozie@gmail.com>
2023-02-28 20:17:15 +0300

    Remove unnecessary files & update generate-rsa.py

--- generate-rsa.py
+++ generate-rsa.py
@@ -1,10 +1,16 @@
+import dotenv
 from Crypto.PublicKey import RSA  # provided by pycryptodome
+try:
+    dotenv_file = dotenv.find_dotenv()
+    dotenv.load_dotenv(dotenv_file)
+    key = RSA.generate(4096)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
 
-key = RSA.generate(2048)
-private_key = key.export_key()
-public_key = key.publickey().export_key()
+    # Write changes to .env file.
+    dotenv.set_key(dotenv_file, "PRIVATE_KEY", private_key.decode())
+    dotenv.set_key(dotenv_file, "PUBLIC_KEY", public_key.decode())
 
-print('private key:')
-print(private_key.decode())
-print('public key:')
-print(public_key.decode())
+    print('PRIVATE AND PUBLIC KEYS were successfully generated and added in environment variables')
+except FileNotFoundError:
+    print("No .env file found")
